65b52062aaededcc5602cc72730a3ecaba0ae1cb
ia17011 <cfoyvlird@gmail.com>
2020-12-30 22:29:48 +0900

    feat: 🎸 i18n wroking at Settings.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,26 @@ import {Routes} from './router/Routes'
 import {GeistProvider, CssBaseline} from '@geist-ui/react'
 import {ThemeConfigProvider} from './utils/theme-config-provider'
 import {RecoilRoot} from 'recoil'
+import i18n from 'i18next'
+import {initReactI18next} from 'react-i18next'
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: {
+        Language: 'Language',
+      },
+    },
+    ja: {
+      translation: {
+        Language: '言語',
+      },
+    },
+  },
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: {escapeValue: false},
+})
 
 export const App = () => {
   const [themeType, setThemeType] = React.useState('dark')
--- src/app/pages/Settings/Internal/Languages.tsx
+++ src/app/pages/Settings/Internal/Languages.tsx
@@ -1,17 +1,35 @@
 import {Card, Divider, Radio, Text} from '@geist-ui/react'
 import React from 'react'
+import i18n from 'i18next'
+import {useTranslation} from 'react-i18next'
 
-export const Languages: React.VFC = (props) => (
-  <Card style={{margin: '20px 0px'}}>
-    <Card.Content>
-      <Text h4>Language</Text>
-    </Card.Content>
-    <Divider y={0} />
-    <Card.Content>
-      <Radio.Group value="1" useRow>
-        <Radio value="1">English</Radio>
-        <Radio value="2">日本語</Radio>
-      </Radio.Group>
-    </Card.Content>
-  </Card>
-)
+export const Languages: React.VFC = (props) => {
+  const [lang, setLang] = React.useState('')
+  const [t, i18n] = useTranslation()
+
+  React.useEffect(() => {
+    i18n.changeLanguage(lang)
+  }, [lang, i18n])
+
+  return (
+    <Card style={{margin: '20px 0px'}}>
+      <Card.Content>
+        <Text h4>{t('Language')}</Text>
+        <Text>
+          {t('Current Language:')} {lang}
+        </Text>
+      </Card.Content>
+      <Divider y={0} />
+      <Card.Content>
+        <Radio.Group value="1" useRow>
+          <Radio value="1" onClick={() => setLang('en')}>
+            English
+          </Radio>
+          <Radio value="2" onClick={() => setLang('ja')}>
+            日本語
+          </Radio>
+        </Radio.Group>
+      </Card.Content>
+    </Card>
+  )
+}
