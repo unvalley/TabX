7a50d223f35bad8e33b7a2b080c1509d067bdcd4
hiroki.ihoriya <cfoyvlird@gmail.com>
2021-02-21 18:24:00 +0900

    refactor: 💡 storage.ts

--- src/shared/storage.ts
+++ src/shared/storage.ts
@@ -11,44 +11,42 @@ import { genParamsToFetchMetadata, zip } from './utils/util'
 
 const mutex = new Mutex()
 
-const get = (storageKey: string) => browser.storage.local.get(storageKey)
+const get = (key: string) => browser.storage.local.get(key)
 const set = (obj: Record<string, unknown>) => browser.storage.local.set(obj)
 
 // ========================
 // Storage Operations
 // ========================
 
-export const getAllLists = async <T extends ListName>(storageKey: T): Promise<ListType<T>[]> => {
-  const cachedData = loadCache(storageKey)
+export const getAllLists = async <T extends ListName>(key: T): Promise<ListType<T>[]> => {
+  const cachedData = loadCache(key)
   if (cachedData.length > 0) {
     return cachedData
   }
-  const res = await get(storageKey).then(data =>
-    Array.isArray(data[storageKey]) ? (data[storageKey] as ListType<T>[]) : [],
-  )
-  saveCache(storageKey, res)
+  const res = await get(key).then(data => (Array.isArray(data[key]) ? (data[key] as ListType<T>[]) : []))
+  saveCache(key, res)
   return res
 }
 
-export const setLists = (storageKey: ListName, lists: TabList[] | DomainTabList[]) => {
-  saveCache(storageKey, [])
-  return set({ [storageKey]: lists })
+export const setLists = (key: ListName, lists: TabList[] | DomainTabList[]) => {
+  saveCache(key, [])
+  return set({ [key]: lists })
 }
 
-export const addList = async (storageKey: ListName, newList: TabList | DomainTabList) => {
-  const allTabLists = await getAllLists(storageKey)
+export const addList = async (key: ListName, newList: TabList | DomainTabList) => {
+  const allTabLists = await getAllLists(key)
   const updatedAllTabLists = produce(allTabLists, draft => {
     draft.push(newList)
   })
-  return setLists(storageKey, updatedAllTabLists)
+  return setLists(key, updatedAllTabLists)
 }
 
-export const addLists = async (storageKey: ListName, newLists: TabList[]) => {
-  const allTabLists = await getAllLists(storageKey)
+export const addLists = async (key: ListName, newLists: TabList[]) => {
+  const allTabLists = await getAllLists(key)
   const updatedAllTabLists = produce(allTabLists, draft => {
     return [...draft, ...newLists]
   })
-  return setLists(storageKey, updatedAllTabLists)
+  return setLists(key, updatedAllTabLists)
 }
 
 export const deleteAllLists = (key: string) => set({ [key]: null })
@@ -61,26 +59,22 @@ export const addDomainTabs = async (groupedNewList: [Domain, TabSimple[]][]) => 
     const allDomainTabLists = await getAllLists(DOMAIN_TAB_LISTS)
     const domains = allDomainTabLists.map(list => list.domain)
 
-    console.log('domains', domains)
     const updatedAllTabLists = await produce(allDomainTabLists, async draft => {
       draft.forEach(list => {
         groupedNewList.forEach(async newList => {
           const domain = newList[0]
           const domainTabList = newList[1]
           if (list.domain === domain) {
-            // exisiting domain
-            console.log('domain matches', list.domain, domain)
             list.tabs.push(...domainTabList)
           }
           if (!domains.includes(domain)) {
-            console.log('domain not includes', domain)
             draft.push(createNewDomainTabList(domain, domainTabList))
+            // update domains for considlering loop
             domains.push(domain)
           }
         })
       })
     })
-    console.log('updateAllTabLists', updatedAllTabLists)
     setLists(DOMAIN_TAB_LISTS, updatedAllTabLists)
   } catch (err) {
     console.error(err)
@@ -93,21 +87,21 @@ export const addDomainTabs = async (groupedNewList: [Domain, TabSimple[]][]) => 
  * Delete Single Tab Link in a TabList
  * @param id
  */
-export const deleteTabLink = async (storageKey: ListName, tabListId: number, tabId: number) => {
+export const deleteTabLink = async (key: ListName, tabListId: number, tabId: number) => {
   const release = await mutex.acquire()
   try {
     // SELECT
-    const allTabLists = await getAllLists(storageKey)
+    const allTabLists = await getAllLists(key)
     const updatedAllTabLists = produce(allTabLists, draft => {
       const targetTabListElem = draft.filter(list => list.id === tabListId)[0]
       const idx = targetTabListElem.tabs.findIndex(({ id }) => id === tabId)
       targetTabListElem.tabs = targetTabListElem.tabs.filter((_, i) => i !== idx)
       // DELETE and hanlde if tabs are empty
-      !targetTabListElem.tabs.length && deleteTabList(storageKey, tabListId)
+      !targetTabListElem.tabs.length && deleteTabList(key, tabListId)
     })
     // UPDATE
     console.log('updateALlTabLists', updatedAllTabLists)
-    setLists(storageKey, updatedAllTabLists)
+    setLists(key, updatedAllTabLists)
   } catch (err) {
     console.error(err)
   } finally {
@@ -119,23 +113,23 @@ export const deleteTabLink = async (storageKey: ListName, tabListId: number, tab
  * Delete TabList
  * @param tabListId
  */
-export const deleteTabList = async (storageKey: ListName, tabListId: number) => {
+export const deleteTabList = async (key: ListName, tabListId: number) => {
   const release = await mutex.acquire()
   try {
-    const allTabLists = await getAllLists(storageKey)
+    const allTabLists = await getAllLists(key)
     const updatedAllTabLists = produce(allTabLists, draft => {
       const listIdx = draft.findIndex(({ id }) => id === tabListId)
       draft.splice(listIdx, 1)
     })
-    setLists(storageKey, updatedAllTabLists)
+    setLists(key, updatedAllTabLists)
   } finally {
     release()
   }
 }
 
-export const pinnTabList = async (storageKey: ListName, tabListId: number) => {
+export const pinnTabList = async (key: ListName, tabListId: number) => {
   // SELECT
-  const allTabLists = await getAllLists(storageKey)
+  const allTabLists = await getAllLists(key)
   const targetTabListElem = allTabLists.filter(list => list.id === tabListId)[0]
 
   // UPDATE
@@ -143,14 +137,14 @@ export const pinnTabList = async (storageKey: ListName, tabListId: number) => {
   setLists(TAB_LISTS, allTabLists)
 }
 
-export const restoreTabList = async (storageKey: ListName, tabListId: number) => {
+export const restoreTabList = async (key: ListName, tabListId: number) => {
   // SELECT
-  const allTabLists = await getAllLists(storageKey)
+  const allTabLists = await getAllLists(key)
   const targetTabListElem = allTabLists.filter(list => list.id === tabListId)[0]
   // OPEN
   await restoreTabs(targetTabListElem.tabs)
   // DELETE
-  await deleteTabList(storageKey, tabListId)
+  await deleteTabList(key, tabListId)
 }
 
 // ========================
