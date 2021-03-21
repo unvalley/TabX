0ca1495b9ddeb4daa09fabdc8c153c453a21ea64
hiroki.ihoriya <cfoyvlird@gmail.com>
2021-03-22 00:38:00 +0900

    refactor: 💡 load more

--- src/app/pages/List/List.tsx
+++ src/app/pages/List/List.tsx
@@ -1,8 +1,10 @@
 import { Button } from '@geist-ui/react'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { Header } from '~/app/components/organisms/Header'
 import { TabListContainer } from '~/app/components/organisms/TabList'
+import { useLoadMore } from '~/app/hooks/useLoadMore'
+import { useLocalStorage } from '~/app/hooks/useLocalStorage'
 // import { useLocalStorage } from '~/app/hooks/useLocalStorage'
 import { TabList } from '~/shared/typings'
 
@@ -23,46 +25,25 @@ const MemoizedTabGroups = React.memo<{
 
 MemoizedTabGroups.displayName = 'MemoizedTabGroups'
 
-const PER_PAGE = 2
-
 export const List: React.FC<Props> = props => {
   const { tabLists } = props
   const { t } = useTranslation()
 
-  // const [shouldShowTabListHeader] = useLocalStorage<boolean>('shouldShowTabListHeader')
-  // const { itemsToShow, handleShowMorePosts } = useLoadMore(PER_PAGE, tabLists)
-  const [limit, setLimit] = useState(PER_PAGE)
-  const [itemsToShow, setItemsToShow] = useState<TabList[]>([])
+  const [shouldShowTabListHeader] = useLocalStorage<boolean>('shouldShowTabListHeader')
+  const perCount = 8
+  const { itemsToShow, handleShowMoreItems } = useLoadMore(perCount, tabLists)
 
-  const loopWithSlice = (start: number, end: number) => {
-    const slicedItems = tabLists.slice(start, end)
-    const newItems = [...itemsToShow, ...slicedItems]
-    setItemsToShow(newItems)
-  }
-
-  useEffect(() => {
-    loopWithSlice(0, PER_PAGE)
-  }, [])
-
-  const handleShowMorePosts = () => {
-    loopWithSlice(limit, limit + PER_PAGE)
-    setLimit(limit + PER_PAGE)
-  }
-
-  // const currentPageItems = tabLists.slice(offset, offset + PER_PAGE).map((item, idx) => {
-  //   return <TabListContainer key={item.id} idx={calcIdx(idx)} shouldShowTabListHeader={shouldShowTabListHeader} />
-  // })
-  console.log(itemsToShow)
+  const currentItems = itemsToShow.map((item, idx) => (
+    <TabListContainer key={`${item.id}_${idx}`} idx={idx} shouldShowTabListHeader={shouldShowTabListHeader} />
+  ))
 
   return (
     <>
       <Header />
       {tabLists.length > 0 ? (
         <>
-          {itemsToShow.map((item, idx) => (
-            <TabListContainer key={item.id + new Date().getTime()} idx={idx} shouldShowTabListHeader={true} />
-          ))}
-          <Button onClick={handleShowMorePosts}>loadMore</Button>
+          {currentItems}
+          <Button onClick={handleShowMoreItems}>loadMore</Button>
         </>
       ) : (
         <h4>{t('TAB_LISTS_EMPTY_MESSAGE')}</h4>
