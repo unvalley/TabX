180e84fa6eecd19bd0305e0da1c500526fb81df2
Hiroki.Ihoriya <38400669+unvalley@users.noreply.github.com>
2021-08-29 20:01:53 +0900

    chore: delete domain and mappers (#106)

--- src/core/domain/tabTitle.ts
+++
@@ -1,8 +0,0 @@
-import { omitText } from '../shared/utils'
-import { ValueObject } from '../shared/utils/valueObject'
-
-export class TabTitle extends ValueObject<string> {
-  public omitTitle(length: number) {
-    return omitText(this.val)(length)('…')
-  }
-}
