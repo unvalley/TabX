4d962dd984c97e347832426896ea52e4bf7dea3a
unvalley <kirohi.code@gmail.com>
2021-08-29 20:20:24 +0900

    chore: use local storage and system color

--- src/ui/stores/colorTheme.ts
+++ src/ui/stores/colorTheme.ts
@@ -2,6 +2,8 @@ import { atom, selector } from 'recoil'
 
 import { Themes } from '../constants/styles'
 
+const isSystemColorDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+
 export const colorThemeState = atom<string>({
   key: 'colorThemeState',
   default: selector({
@@ -9,7 +11,7 @@ export const colorThemeState = atom<string>({
     get: () => {
       const theme = localStorage.getItem('theme')
       if (theme === null) {
-        return Themes.LIGHT
+        return isSystemColorDark ? Themes.DARK : Themes.LIGHT
       }
       return theme
     },
