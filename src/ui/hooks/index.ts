2e0ab03574ae25d96e1d8d709dbece895f725f99
unvalley <kirohi.code@gmail.com>
2021-09-12 16:41:13 +0900

    chore: import

--- src/ui/hooks/index.ts
+++ src/ui/hooks/index.ts
@@ -1,5 +1,6 @@
+import { useFuse } from './useFuse'
 import { useLoadMore } from './useLoadMore'
 import { useLocalStorage } from './useLocalStorage'
 import { useMouseOver } from './useMouseOver'
 
-export { useLoadMore, useLocalStorage, useMouseOver }
+export { useLoadMore, useLocalStorage, useMouseOver, useFuse }
