6270e39e9724ef9bb2fec10316f75df8431ad5eb
unvalley <kirohi.code@gmail.com>
2022-04-21 22:37:26 +0900

    wip: version up

---
+++ tailwind.config.js
@@ -0,0 +1,60 @@
+module.exports = {
+  important: true,
+  // Active dark mode on class basis
+  darkMode: 'class',
+  i18n: {
+    locales: ['en-US'],
+    defaultLocale: 'en-US'
+  },
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        check: "url('/icons/check.svg')",
+        landscape: "url('/images/landscape/2.jpg')"
+      })
+    }
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['checked'],
+      borderColor: ['checked'],
+      inset: ['checked'],
+      zIndex: ['hover', 'active']
+    }
+  },
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  daisyui: {
+    themes: [
+      'light',
+      'dark',
+      'cupcake',
+      'bumblebee',
+      'emerald',
+      'corporate',
+      'synthwave',
+      'retro',
+      'cyberpunk',
+      'valentine',
+      'halloween',
+      'garden',
+      'forest',
+      'aqua',
+      'lofi',
+      'pastel',
+      'fantasy',
+      'wireframe',
+      'black',
+      'luxury',
+      'dracula',
+      'cmyk',
+      'autumn',
+      'business',
+      'acid',
+      'lemonade',
+      'night',
+      'coffee',
+      'winter'
+    ]
+  }
+}
